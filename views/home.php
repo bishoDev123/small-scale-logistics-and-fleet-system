aa25ea52b09f510aab401d20117a6ea662e4a71d
bishoDev123 <bisholiveenglish@gmail.com>
2026-05-07 19:29:40 +0300

    added a login and logout buttons

--- views/home.php
+++ views/home.php
@@ -1,9 +1,37 @@
 <h1>Users</h1>
 
-<?php
-foreach ($users as $user): ?>
-    <p><?= $user['name'] ?></p>
-        <li>email: <?=$user['email']?> </li>
-        <li>password: <?=$user['password']?></li>
+<?php foreach ($users as $user): ?>
+
+    <p>
+        <?= $user['name'] ?>
+    </p>
+
+    <ul>
+        <li>
+            Email:
+            <?= $user['email'] ?>
+        </li>
+
+        <li>
+            Role:
+            <?= $user['role_name'] ?>
+        </li>
     </ul>
+
 <?php endforeach; ?>
+
+<br>
+
+<a href="index.php?url=auth/login">
+    <button>
+        Login
+    </button>
+</a>
+
+<br><br>
+
+<a href="index.php?url=auth/logout">
+    <button>
+        Logout
+    </button>
+</a>
